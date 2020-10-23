1dfe1097419831c9104772e16a2a579e4cabf45b
Kyle Allen <Kallen478@gmail.com>
2020-10-23 02:38:10 -0400

    added variables with arrays of characters

--- script.js
+++ script.js
@@ -9,70 +9,30 @@ function writePassword() {
 
   passwordText.value = password;
 }
-  // generatePassword function should be below
 
-  // ******* function logic starts here *******
-  // THEN I am presented with a series of prompts for password criteria
-  
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-  
-  // ******* 1 PROMPT *******
-  // ******* REQUIRES VALIDATION *******
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // we need a prompt that asks for the passoword length from the user
-  // check if the input is < 8 and > 128
+// arrays of possible characters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+// function to generate the password
+// function generatePassword() {
   var passwordLength = prompt("How many characters do you want to have in your password?");
+    // validation for password length
     if(passwordLength < 8 || passwordLength > 128) {
-        alert("Please choose a password length of 8-128 characters.");
+    alert("Please choose a password length of 8-128 characters.");
+    
     }
-    // else if(passwordLength < 129){
-    //     alert("Your password will be " + passwordLength + " characters long.");
-    // }
     else {
-        alert("Please select criteria for your password");
+    // choose password criteria
+    var criteria;
+    var confirmLowercase = confirm("Would you like to use any lowercase letters?");
+    var confirmUppercase = confirm("Would you like to use any uppercase letters?");
+    var confirmNumeric = confirm("Would you like to use any numbers?");
+    var confirmSpecial = confirm("Would you like to use any special characters?");
     }
-
-
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  // each prompt requires validation
-  // probably use a function to turn the length(string) into a number
-
-  // ******* 4 COMFIRMS *******
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  // we need a confirm that asks the user if they want to use lowercase
-  // we need a confirm that asks the user if they want to use uppercase
-  // we need a confirm that asks the user if they want to use numeric
-  // we need a confirm that asks the user if they want to use special characters
-  // 1 collectionValidCharacters - array of numbers, letters and characters that can be used.
-  // 2 computer picks a random valid character 8 times and goes in a variable called password
-  // 3 collectionValidCharacters[Math.floor(Math.random() * collectionValidCharacters.length)]
-  // 4 var myrandomness = "";
-  // 5 myrandomness = myrandomness + collectionValidCharacters[Math.floor(Math.random() * collectionValidCharacters.length)]
-  
-  // ******* end of data collection *******
-  // using the number from above and the 4 booleans
-  
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // all the info is gathered (went through all prompts)
-  // all the HARD logic goes here
-
-
-  // WHEN the password is generated
-  // THEN the password is either displayed in an alert or written to the page
-  // do an alert with the password, or just check to see is my variable has a password
-
-
-  // ******* end of function logic *******
-
-
-
-
-
-// WHEN I click the button to generate a password
+    
+// }  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
